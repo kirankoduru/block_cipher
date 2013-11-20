9c8241177af991e4cf669b897af8327d4de42deb
rockyxshen <rockyxshen@gmail.com>
2013-11-19 22:10:14 -0500

    Update aes.c

--- aes.c
+++ aes.c
@@ -5,10 +5,6 @@
 
 #define AES_BLOCK_SIZE 16
 
-/**
- * Create an 256 bit key and IV using the supplied key_data. salt can be added for taste.
- * Fills in the encryption and decryption ctx objects and returns 0 on success
- **/
 int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt,
              EVP_CIPHER_CTX *e_ctx, EVP_CIPHER_CTX *d_ctx)
 {
